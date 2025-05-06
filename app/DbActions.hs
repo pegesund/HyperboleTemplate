6e6fb08f4fc445abc9d72790f736c23fe2b5b834
Petter <petter.egesund@gmail.com>
2025-05-06 23:01:23 +0200

    code cleanup

--- app/DbActions.hs
+++ app/DbActions.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators #-}
 
 module DbActions (
